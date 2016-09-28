fd9829ab11ee0e126ee5c835cca670180d0bd4d2
Multipixelone <finn@cnwr.net>
2016-09-28 13:10:57 -0700

    Create PyttsxTest.py
    Another Test :)

---
+++ PyttsxTest.py
@@ -0,0 +1,5 @@
+import pyttsx
+engine = pyttsx.init()
+engine.say('Welcome to Remote! Press the center button to get a reading of the object in front of you')
+engine.say('Press the button for more information')
+engine.runAndWait()
