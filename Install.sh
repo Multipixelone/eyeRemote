ffbf94e61d775d4751f4d87337fabcb7300932d2
Multipixelone <finn@cnwr.net>
2016-10-07 15:09:09 -0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,8 +9,9 @@ apt-get install portaudio19-dev
 pip install tts-watson
 apt-get install python-pyaudio
 apt-get install caca-utils
-cd /usr/local
-wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
-tar -xvf go1.7.1.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+#cd /usr/local
+#wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
+#tar -xvf go1.7.1.linux-amd64.tar.gz
+#echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+sudo apt-get install gccgo-go
 go get github.com/ichinaski/pxl
