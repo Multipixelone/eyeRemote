a85eefab7affcc54e82c1f371e2e1a3e542c81cc
Multipixelone <finn@cnwr.net>
2016-10-15 18:48:26 -0700

    Added PiCamera

--- Install.sh
+++ Install.sh
@@ -1,4 +1,5 @@
 # Install Dependances
+apt-get update
 apt-get install python-pip
 pip install pyttsx
 apt-get install espeak
@@ -9,6 +10,7 @@ apt-get install portaudio19-dev
 pip install tts-watson
 apt-get install python-pyaudio
 apt-get install caca-utils
+apt-get install python-picamera
 #cd /usr/local
 #wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
 #tar -xvf go1.7.1.linux-amd64.tar.gz
