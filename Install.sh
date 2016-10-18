38fa27bd7947dff719b5f2c13622d0d6cb44070e
Multipixelone <finn@cnwr.net>
2016-10-17 19:13:43 -0700

    Make the File Fully Automatic :)

--- Install.sh
+++ Install.sh
@@ -1,21 +1,21 @@
 # Install Dependances
 apt-get update
-apt-get install python-pip
+apt-get --yes --force-yes install python-pip
 pip install pyttsx
-apt-get install espeak
+apt-get --yes --force-yes install espeak
 pip install jupyter
 pip install gTTS
-apt-get install python-pygame
-apt-get install portaudio19-dev
+apt-get install --yes --force-yes python-pygame
+apt-get install --yes --force-yes portaudio19-dev
 pip install tts-watson
-apt-get install python-pyaudio
-apt-get install caca-utils
-apt-get install python-picamera
+apt-get install --yes --force-yes python-pyaudio
+apt-get install --yes --force-yes caca-utils
+apt-get install --yes --force-yes python-picamera
 #cd /usr/local
 #wget https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz
 #tar -xvf go1.7.1.linux-amd64.tar.gz
 #echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
-sudo apt-get install gccgo-go
+sudo apt-get --yes --force-yes install gccgo-go
 go get github.com/ichinaski/pxl
 pip install schedule
 pip install --upgrade google-api-python-client
