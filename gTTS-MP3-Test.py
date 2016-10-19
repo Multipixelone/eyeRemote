324e0a184d1466e2b71d85980467d13f3ea0a09c
Multipixelone <finn@cnwr.net>
2016-10-18 20:03:06 -0700

    Just testing this out...

--- gTTS-MP3-Test.py
+++ gTTS-MP3-Test.py
@@ -1,11 +1,11 @@
 # Load gTTS, which allows Text to Speech
 from gtts import gTTS
 # Load Mixer, for Playing the mp3 file
-from pygame import mixer
+import pygame
 
 tts = gTTS(text='Welcome to the Remote! Press the Button for Information of what your looking at!', lang='en')
 tts.save("Welcome.mp3")
 
-mixer.init()
-mixer.music.load('Welcome.mp3')
-mixer.music.play()
+pygame.mixer.init()
+pygame.mixer.music.load('Welcome.mp3')
+pygame.mixer.music.play()
