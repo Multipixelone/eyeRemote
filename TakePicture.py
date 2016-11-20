e7fc7f5182dce7185d599f6b167a1aafdc8c1718
Multipixelone <finn@cnwr.net>
2016-11-20 14:15:31 -0800

    Added the GPIO Pins

--- TakePicture.py
+++ TakePicture.py
@@ -1,4 +1,11 @@
 import picamera
 
+from LocalVariables import takepicture
+
+GPIO.setmode(GPIO.BCM)
 camera = picamera.PiCamera()
-camera.capture('image.jpg')
+GPIO.setup(takepicture, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    GPIO.wait_for_edge(gpio_pin_number, GPIO.FALLING)
+    camera.capture('image.jpg')
