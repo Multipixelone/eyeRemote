ed273eb13bea643fabe1a29bb0e481f7f4f3a5ae
Multipixelone <finn@cnwr.net>
2016-11-20 11:13:55 -0800

    Update and rename APIKey.py to LocalVariables.py

--- APIKey.py
+++
@@ -1,1 +0,0 @@
-# This will be overwritten with the API Key, when it is typed into Install.sh. Thanks :) 
---
+++ LocalVariables.py
@@ -0,0 +1,1 @@
+# This will be overwritten with the API Key, along with other local variables, when it is typed into Install.sh. Thanks :) 
