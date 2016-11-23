6c52858a1d5e3b9c04a9c3266c0a321b2d4346f6
Multipixelone <finn@cnwr.net>
2016-11-22 17:13:43 -0800

    Update ShutdownWhenPress.py

--- ShutdownWhenPress.py
+++ ShutdownWhenPress.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(shutdownpin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 try:
-    GPIO.wait_for_edge(gpio_pin_number, GPIO.FALLING)
+    GPIO.wait_for_edge(shutdownpin, GPIO.FALLING)
     os.system("sudo shutdown -h now")
 except:
     pass
