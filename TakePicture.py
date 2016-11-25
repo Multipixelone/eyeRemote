f0671d41593bf35f4df202d2bbe2ab24e3131a15
Multipixelone <finn@cnwr.net>
2016-11-25 14:44:31 -0800

    Making everything functions

--- TakePicture.py
+++ TakePicture.py
@@ -3,11 +3,7 @@ import RPi.GPIO as GPIO
 
 from LocalVariables import takepicture
 
-GPIO.setmode(GPIO.BCM)
 camera = picamera.PiCamera()
-GPIO.setup(takepicture, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-while True:
-    if (GPIO.input(takepicture)):
-        camera.capture('image.jpg')
-        print('PICTURE TAKEN :D')
+def TakePicture():
+    camera.capture('image.jpg')
