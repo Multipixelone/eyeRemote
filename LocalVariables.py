5b7b3d01e3092c173aa667f4e971e6073316ce92
Multipixelone <finn@cnwr.net>
2016-11-25 18:51:38 -0800

    Update LocalVariables.py

--- LocalVariables.py
+++ LocalVariables.py
@@ -1,4 +1,6 @@
 # This will be overwritten with the API Key, along with other local variables, when it is typed into Install.sh. Thanks :) 
+
 # Default pin values:
+key = 'Enter key here unless you run the script :P'
 shutdownpin = 21
 takepicture = 5
