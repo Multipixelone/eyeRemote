4ed3429472dfc0665bd92406215bcbb7c79719d4
Finn <finn@cnwr.net>
2017-01-17 10:08:09 -0800

    Removed some commented out stuff

--- Install.sh
+++ Install.sh
@@ -2,7 +2,7 @@
 # What this script is doing
 printf "Do you know what this script is doing?\n\nJust checking, but you should really know! This script installs all the dependances required for Multipixelones eyeRemote, and then adds it to startup.\n\nThis will convert your Pi into a talking remote. :)\n\nJust making sure :D\n"
 
-# You sure man?
+# You sure?
 read -r -p "Proceed with Install? [y/N]"
 echo
 if [[ ! $REPLY =~ ^[Nn]$ ]]
@@ -11,17 +11,12 @@ then
     apt-get --yes --force-yes upgrade
     raspi-config nonint do_camera 0
     apt-get --yes --force-yes install python-pip
-    # pip install pyttsx
-    # apt-get --yes --force-yes install espeak
-    # pip install jupyter
     pip install gTTS
     apt-get --yes --force-yes install mpg123
     pip install sultan
     apt-get install --yes --force-yes caca-utils
     apt-get install --yes --force-yes python-picamera
-    #cd /usr/local
     pip install schedule
-    # pip install --upgrade google-api-python-client
     pip install cloudsight
     echo -n "Enter your Cloudsight API Key [ENTER]: "
     read key
@@ -39,12 +34,7 @@ then
     echo "takepicture = $tpicturepin" >> LocalVariables.py
     printf "\n\n\n\n\nInstall Complete!\n Adding to Startup..."
     touch tmpcron
-    #sed -i '1ipython /home/pi/BlindRemote/ShutdownWhenPress.py &' /etc/rc.local
     sed -i '1i./home/pi/BlindRemote/start.sh &' /etc/rc.local
-    #echo "@reboot python /home/pi/BlindRemote/ShutdownWhenPress.py &" >> tmpcron
-    #echo "@reboot sh /home/pi/BlindRemote/start.sh &" >> tmpcron
-    #crontab tmpcron
-    #rm tmpcron
     sleep 1
     printf "\nNow Rebooting...\n"
     sleep 1
